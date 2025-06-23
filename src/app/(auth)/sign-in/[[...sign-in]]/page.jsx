1ea897c86ddaaff2afe4ff3bc158f304580833c4
Shubham Bisht <shubhambisht841@gmail.com>
2025-06-23 23:41:54 +0530

    update sign in

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -4,7 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 const Page = () => {
   return (
     <div className="flex items-center justify-center px-4">
-      <div className="flex flex-row items-start gap-8">
+      <div className="flex flex-col md:flex-row items-center md:items-start gap-8">
         {/* Dummy Email/Password Box */}
         <DummyAlert disabled/>
 
