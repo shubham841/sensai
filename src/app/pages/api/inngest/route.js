0640175b598843032dd9ef80be90f6b8aecfe909
Shubham Bisht <shubhambisht841@gmail.com>
2025-06-23 11:18:01 +0530

    some changes in the versions

--- src/app/pages/api/inngest/route.js
+++ src/app/pages/api/inngest/route.js
@@ -1,12 +1,23 @@
+// import { inngest } from "@/lib/inngest/client";
+// import { generateIndustryInsights } from "@/lib/inngest/functions";
+// import { serve } from "inngest/next";
+
+// // Create an API that serves zero functions
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [
+//     /* your functions will be passed here later! */
+//     generateIndustryInsights,
+//   ],
+// });
+
+// pages/api/inngest.ts
 import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
+export default serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-    generateIndustryInsights,
-  ],
+  functions: [generateIndustryInsights],
 });
+
